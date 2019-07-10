4d4e5058f338e3f1ae0a4df9c3dff171c9749a5f
Terushi Tada <s1260097@std1dc39.u-aizu.ac.jp>
2019-07-10 23:20:41 +0900

    first revision

--- saikoro.c
+++ saikoro.c
@@ -1,23 +1,22 @@
-/*
- * C言語のサンプルプログラム - Webkaru
- * - 乱数 - サイコロをつくってみよう - 
- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
  
 int main(void){
  
-  int s;
+  int s,t,h;
  
   /* 乱数の種を初期化 */
   srand(time(NULL));
- 
+  printf("Rolling the dice...\n");
   /* サイコロを振る */
   s = rand() % 6 + 1;
+  t = rand() % 6 + 1;
+  h = s+t;
  
   /* サイコロの出目を出力 */
-  printf("サイコロの出目 = %d \n", s);
+  printf("Die 1: %d\nDie 2: %d\nTotal value: %d \n", s,t,h);
  
   return 0;
 }
