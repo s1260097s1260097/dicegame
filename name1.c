e8413e15b65dda210a66eb6d75f543e8e7f48f2f
Terushi Tada <s1260097@std1dc39.u-aizu.ac.jp>
2019-07-10 23:40:27 +0900

    Won/lost message added

---
+++ name1.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+
+ 
+int main(void){
+ 
+  char *n;
+ 
+    printf("What is your name?\n");
+     printf(">");
+ 
+    scanf("%s",n);
+    printf("Hello, %s!\n",n);
+ 
+  return 0;
+}
